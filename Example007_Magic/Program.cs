369628b11fecac75641bbac39e2f39eef8d27e76
Sergey <tyosergey.a@gmail.com>
2023-10-07 01:04:51 +0500

    final result

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,15 +2,12 @@
 // Console.SetCursorPosition(10, 4);
 // Console.WriteLine("+");
 
-int xa = 1;
-int ya = 1;
-int xb = 1;
-int yb = 30;
-int xc = 40;
-int yc = 30;
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+"); 
+Console.WriteLine("+");
 
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
@@ -19,26 +16,26 @@ Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
 int x = xa, y = xb;
-int count  = 0;
-while(count < 10)
+int count = 0;
+while (count < 10000)
 {
     int what = new Random().Next(0, 3);
-    if(what == 0)
+    if (what == 0)
     {
-        x = (x+xa)/2;
-        y = (y+ya)/2;
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
     }
 
-    if(what==1)
+    if (what == 1)
     {
-        x = (x+xb)/2;
-        y = (y+yb)/2;
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
     }
 
-    if(what==2)
+    if (what == 2)
     {
-        x = (x+xc)/2;
-        y = (y+yc)/2;
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
     }
 
     Console.SetCursorPosition(x, y);
